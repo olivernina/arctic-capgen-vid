0459d50082fb98901aba9a0fd5feafe05c955dfb
ryan <pk_blaze@knights.ucf.edu>
2016-02-27 20:15:19 -0500

    caffe

--- config.py
+++ config.py
@@ -81,4 +81,9 @@ config = DD({
         'reload_': False,
         'dec':'multi-random'
         }),
+    'noinput': DD({
+        'save_model_dir': exp_path + 'arctic-capgen-vid/test_non/',
+        'reload_': False,
+        'dec':'multi-random'
+        }),
     })

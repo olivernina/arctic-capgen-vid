f2abd348d71920ac0a08c90b5f4118f3e4f13579
ryan <pk_blaze@knights.ucf.edu>
2016-03-02 14:44:27 -0500

    caffe

--- config.py
+++ config.py
@@ -101,4 +101,12 @@ config = DD({
         'encoder_dim': 200,
         # 'sampleFreq':1,
         }),
+    'clstm': DD({
+        'save_model_dir': exp_path + 'arctic-capgen-vid/test_non/',
+        'reload_': False,
+        'dec':'multi-stdist',
+        'encoder':'lstm_uni',
+        'encoder_dim': 200,
+        # 'sampleFreq':1,
+        }),
     })

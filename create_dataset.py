083bd2953ec1bdbc05ae95b0999f590a0ae8f543
oliver <olivernina@gmail.com>
2016-06-06 16:14:55 -0400

    icoupled

--- create_dataset.py
+++ create_dataset.py
@@ -1043,7 +1043,7 @@ def vtt_c3d(params):
 
     test_mode = params['test']
     if test_mode:
-        train_val_list_path = 'train_val_annotation/train_val_videodatainfo.json'
+        train_val_list_path = 'annotations/train_val_videodatainfo.json'
         test_list_path = 'annotations/test_videodatainfo_nosen.json'
 
     else:
